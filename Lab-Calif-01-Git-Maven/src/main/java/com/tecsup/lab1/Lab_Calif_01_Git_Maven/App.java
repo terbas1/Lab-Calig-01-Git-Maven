d335459a2deab2bfbb7ef86240cd14def6836982
Baltazar <edwinbaltazar1996@gmail.com>
2018-09-07 10:03:04 -0500

    Original

---
+++ Lab-Calif-01-Git-Maven/src/main/java/com/tecsup/lab1/Lab_Calif_01_Git_Maven/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab1.Lab_Calif_01_Git_Maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Lab-Calif-01-Git-Maven/src/main/java/com/tecsup/lab1/Lab_Calif_01_Git_Maven/UserView.java
@@ -0,0 +1,5 @@
+package com.tecsup.lab1.Lab_Calif_01_Git_Maven;
+
+public class UserView {
+
+}
